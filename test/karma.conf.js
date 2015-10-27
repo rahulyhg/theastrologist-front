b3befb47a0efe54cb88c105c09ee0f37dff50589
Samy <samyamirou@gmail.com>
2015-10-27 23:31:47 +0100

    chore(timeline): Ajout TU sur le service timeline et gestion du cache

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -17,8 +17,10 @@ module.exports = function(config) {
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
       'app/common/src/**/*.js',
-      'test/**/*.js'
+      'app/partial/**/*.js',
+      'test/**/*-spec.js'
     ],
 
 
